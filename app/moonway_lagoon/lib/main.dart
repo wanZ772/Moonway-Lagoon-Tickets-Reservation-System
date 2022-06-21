ddfa303aa9daf08d0cbdd2f876f5f7e65d153e95
wanZ772 <najwanhassan03@gmail.com>
2022-06-22 02:36:55 +0800

    mobile app frontend code added

--- app/moonway_lagoon/lib/main.dart
+++ app/moonway_lagoon/lib/main.dart
@@ -1,115 +1,285 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MainPage()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  int adults = 0;
+  int kids = 0;
+  var price;
+  bool isMalaysian = false;
+  bool isNotMalaysian = false;
+  bool citizenship = false;
+  @override
+  void calculate_price(adults, kids, citzenship) {
+    print("calculate trigered");
+    print("$adults, $kids, $citizenship");
+    switch (citizenship) {
+      case false:
+        setState(() {
+          if (adults == 2 && kids == 2) {
+            price = 120;
+          } else {
+            price = (50 * kids) + (85 * adults);
+          }
+        });
+        break;
+      case true:
+        setState(() {
+          if (adults == 2 && kids == 2) {
+            price = 100;
+          } else {
+            price = (40 * kids) + (75 * adults);
+          }
+        });
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+        break;
+    }
+    print(price);
   }
 
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            './assets/background.jpg',
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+              top: 40,
+              child: Text(
+                "Moonway Lagoon",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              )),
+          Positioned(
+              top: 120,
+              child: Wrap(
+                alignment: WrapAlignment.spaceAround,
+                spacing: 20,
+                children: [
+                  Container(
+                    child: IconButton(
+                      onPressed: () => setState(() {
+                        if (adults > 0) {
+                          adults--;
+                          calculate_price(adults, kids, citizenship);
+                        }
+                      }),
+                      icon: Icon(Icons.exposure_minus_1),
+                      color: Colors.white,
+                    ),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Adults: $adults",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    width: 130,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.8),
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  Container(
+                    child: IconButton(
+                      onPressed: () => setState(() {
+                        adults++;
+                        calculate_price(adults, kids, citizenship);
+                      }),
+                      icon: Icon(Icons.exposure_plus_1),
+                      color: Colors.white,
+                    ),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                ],
+              )),
+          Positioned(
+              top: 220,
+              child: Wrap(
+                alignment: WrapAlignment.spaceAround,
+                spacing: 20,
+                children: [
+                  Container(
+                    child: IconButton(
+                      onPressed: () => setState(() {
+                        if (kids > 0) {
+                          kids--;
+                          calculate_price(adults, kids, citizenship);
+                        }
+                      }),
+                      icon: Icon(Icons.exposure_minus_1),
+                      color: Colors.white,
+                    ),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Kids: $kids",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    width: 130,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.8),
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  Container(
+                    child: IconButton(
+                      onPressed: () => setState(() {
+                        kids++;
+                        calculate_price(adults, kids, citizenship);
+                      }),
+                      icon: Icon(Icons.exposure_plus_1),
+                      color: Colors.white,
+                    ),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                ],
+              )),
+          Positioned(
+              top: 320,
+              child: Column(
+                children: [
+                  Wrap(
+                    spacing: 100,
+                    children: [
+                      Text(
+                        "Malaysian",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(.8),
+                              border: Border.all(color: Colors.white)),
+                          child: Checkbox(
+                              checkColor: Colors.blue[200],
+                              value: isMalaysian,
+                              onChanged: (bool? value) => setState(() {
+                                    isMalaysian = value!;
+                                    isNotMalaysian = false;
+                                    citizenship = true;
+                                    calculate_price(adults, kids, citizenship);
+                                  })))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Wrap(
+                    spacing: 60,
+                    children: [
+                      Text(
+                        "Non-Malaysian",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(.8),
+                              border: Border.all(color: Colors.white)),
+                          child: Checkbox(
+                              checkColor: Colors.blue[200],
+                              value: isNotMalaysian,
+                              onChanged: (bool? value) => setState(() {
+                                    isNotMalaysian = value!;
+                                    isMalaysian = false;
+                                    citizenship = false;
+                                    calculate_price(adults, kids, citizenship);
+                                  })))
+                    ],
+                  )
+                ],
+              )),
+          Positioned(
+            top: 420,
+            child: Text(
+              (price != null) ? "Total Price: RM$price" : "Total Price: RM0",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Positioned(
+              top: 620,
+              child: Container(
+                width: 250,
+                height: 60,
+                child: FlatButton.icon(
+                  onPressed: () => print(1),
+                  icon: Icon(
+                    Icons.payments_outlined,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    "Make Payment",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.green[400],
+                    border: Border.all(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(20)),
+              ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
